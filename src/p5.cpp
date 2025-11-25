2182c1d547a859f42f8485b374a04243d46c4df3
chang1017 <mitonnaton.mg12@nycu.edu.tw>
2025-11-25 14:28:33 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
+void hanoi(int n, int a, int b, int c) {
+    if (n == 0) return;
+    hanoi(n - 1, a, b, c);
+    cout << "Move disk " << n << " from " << a << " to " << b << "\n";
+    hanoi(n - 1, c, b, a);
+    cout << "Move disk " << n << " from " << b << " to " << c << "\n";
+    hanoi(n - 1, a, b, c);
+}
 
 int main() {
-    
-
+    int n;
+    cin >> n;
+    hanoi(n, 1, 2, 3);
     return 0;
 }
